9b21aeb008c09088882311382754963973228256
thacxuantran <xuanthactk13@gmail.com>
2022-10-13 01:41:41 +0800

    fix semi

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
 
             <main className={styles.main}>
                 <h1 className={styles.title}>
-                    Welcome to <a href="https://nextjs.org">Shyn's World!</a>
+                    Welcome to <a href="https://nextjs.org">Shynn Blog!</a>
                 </h1>
 
                 <p className={styles.description}>
